df54f68f9f72774b8d3a3bbf5f44215ade0e64d1
Vanshika Gupta <vanshikagupta0009@gmail.com>
2025-07-17 13:03:45 +0530

    commit

---
+++ SearchInsertPosition.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class SearchInsertPosition{
+    public int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(nums[mid] == target)
+            return mid;
+            else if(nums[mid] < target)
+            start = mid + 1;
+            else if(nums[mid] > target)
+            end = mid - 1;
+        }
+        return start;
+    }
+    public static void main(String args[]){
+        int[] nums = {1, 3, 5, 6};
+        SearchInsertPosition sol = new SearchInsertPosition();
+        System.out.println(sol.searchInsert(nums,2));
+    }
+}
