a8aea985901bff9cfce1d263d7e0e2c821f5c493
Vanshika Gupta <vanshikagupta0009@gmail.com>
2025-07-18 09:55:25 +0530

    commit

---
+++ MinInArrayList.java
@@ -0,0 +1,24 @@
+package BinarySearch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MinInArrayList {
+    public int findMin(ArrayList<Integer> arr) {
+        int start = 0, end = arr.size() - 1;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (arr.get(mid) > arr.get(end))
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        return arr.get(start);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(4, 5, 6, 7, 0, 1, 2, 3));
+        MinInArrayList sol = new MinInArrayList();
+        System.out.println("Minimum element is: " + sol.findMin(arr));
+    }
+}
---
+++ MinInRotatedSortedArray.java
@@ -0,0 +1,20 @@
+package BinarySearch;
+
+public class MinInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] > nums[end])
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        return nums[start];
+    }
+    public static void main(String args[]){
+        int[] nums = {3,4,5,1,2};
+        MinInRotatedSortedArray sol = new MinInRotatedSortedArray();
+        System.out.println(sol.findMin(nums));
+    }
+}
